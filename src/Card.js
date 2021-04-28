c08fa04251b2e8ad88215cafe0d2efcef3073992
Juli Taras <julibtaras@gmail.com>
2021-04-28 00:07:17 -0300

    Add style

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function MyCard() {
   const [data, setState] = useState(0);
@@ -18,7 +19,10 @@ function MyCard() {
       following: json.following,
       public_repos: json.public_repos,
       public_gists: json.public_gists,
-      repos_url: json.repos_url
+      repos_url: json.repos_url,
+      profile_url: json.html_url,
+      bio: json.bio,
+      company: json.company
 
     };
 
@@ -30,37 +34,38 @@ function MyCard() {
   }, []);
 
   return (
-  
-    <Card style={{ width: '40rem' }} className="text-center">
+    <Card style={{ width: '20rem'}} className="text-center">
     <Card.Img variant="top" src={data.logo} alt="image profile"/>
     <Card.Body>
-      <Card.Title>{data.name}</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted"><Card.Link href="https://github.com/julitaras">{data.profile}</Card.Link></Card.Subtitle>
-      <CardGroup>
-      <Card style={{ width: '10rem' }} className="text-center" bg="secondary" text="white">
-      <Card.Link href="https://github.com/julitaras?tab=repositories">
+      <Card.Title className="mb-2">{data.name}</Card.Title>
+      <Card.Subtitle class="text-muted" style={{ "font-size": '10px'}} className="mb-2">{data.company}</Card.Subtitle>
+      <Card.Text style={{ "font-size": '10px'}} className="pt-3">{data.bio}</Card.Text>
+      <CardGroup className="mb-0">
+      <Card style={{ width: '5rem', height: '4rem' }} className="text-center" bg="secondary">
+      <Card.Link href="https://github.com/julitaras?tab=repositories" class="text-white">
       <Card.Body>
-      <Card.Title>Repos</Card.Title>
-      <Card.Text>{data.public_repos}</Card.Text>
+      <Card.Title style={{ "font-size": '10px'}} className="mb-1">Repos</Card.Title>
+      <Card.Text style={{ "font-size": '10px'}} >{data.public_repos}</Card.Text>
       </Card.Body>
       </Card.Link>
       </Card>
-      <Card style={{ width: '10rem' }} className="text-center" bg="secondary" text="white">
-      <Card.Link href="https://gist.github.com/julitaras">
+      <Card style={{ width: '5rem', height: '4rem' }} className="text-center" bg="secondary">
+      <Card.Link href="https://gist.github.com/julitaras" class="text-white">
       <Card.Body>
-      <Card.Title>Gists</Card.Title>
-      <Card.Text>{data.public_gists}</Card.Text>
+      <Card.Title style={{ "font-size": '10px'}} className="mb-1">Gists</Card.Title>
+      <Card.Text style={{ "font-size": '10px'}}>{data.public_gists}</Card.Text>
       </Card.Body>
       </Card.Link>
       </Card>
-      <Card style={{ width: '10rem' }} className="text-center" bg="secondary" text="white">
+      <Card style={{ width: '5rem', height: '4rem' }} className="text-center" bg="secondary" text="white">
       <Card.Body>
-      <Card.Title>Followers</Card.Title>
-      <Card.Text>{data.followers}</Card.Text>
+      <Card.Title style={{ "font-size": '10px'}} className="mb-1">Followers</Card.Title>
+      <Card.Text style={{ "font-size": '10px'}}>{data.followers}</Card.Text>
       </Card.Body>
       </Card>
       </CardGroup>
     </Card.Body>
+    <Card.Footer className="text-muted"><Card.Link href={data.profile_url} class="text-dark"> <FaGithub/></Card.Link> <Card.Link href="https://www.linkedin.com/in/julieta-taras/" class="text-dark"><FaLinkedin/></Card.Link> <Card.Link href="https://twitter.com/JuliiTaras"class="text-dark"> <FaTwitter/></Card.Link></Card.Footer>
   </Card>
   );
 }
